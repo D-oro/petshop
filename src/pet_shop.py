5a757cbfecbc8a09bf586efedb9f20d7bac41c8f
Dorothea Hinrichs <dorothea.hinrichs@posteo.net>
2022-07-08 12:17:25 +0100

    first two exercises done

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,1 +1,11 @@
 # WRITE YOUR FUNCTIONS HERE
+
+def get_pet_shop_name(pet_shop):
+    name = pet_shop["name"]
+    return name
+
+def get_total_cash(pet_shop):
+    sum = pet_shop["admin"]["total_cash"]
+    return sum
+
+    
