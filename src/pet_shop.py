191aab2fb44a3d0d9b976c82735a7d6a59f2e0ae
Dorothea Hinrichs <dorothea.hinrichs@posteo.net>
2022-07-10 20:20:01 +0100

    still missing three of the main exercises and all optional

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -13,3 +13,41 @@ def add_or_remove_cash(pet_shop, number):
 
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, count):
+    pet_shop["admin"]["pets_sold"] += count
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+# def all_pets_by_breed():
+
+# def all_pets_by_breed_not_found()
+
+def find_pet_by_name(pet_shop, pet):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet:
+            return pet
+
+def remove_pet_by_name(pet_shop, pet):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet:
+            del pet["name"]
+
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
+
+
+def get_customer_cash (customers):
+    return customers["cash"]
+
+
+# def remove_customer_cash (customer, number):
+#     result = (customer["cash"] - number)
+#     return result
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)    
--- tests/pet_shop_test.py
+++ tests/pet_shop_test.py
@@ -101,13 +101,13 @@ class TestPetShop(unittest.TestCase):
         sold = get_pets_sold(self.cc_pet_shop)
         self.assertEqual(0, sold)
 
-    @unittest.skip("delete this line to run the test")
+    
     def test_increase_pets_sold(self):
         increase_pets_sold(self.cc_pet_shop,2)
         sold = get_pets_sold(self.cc_pet_shop)
         self.assertEqual(2, sold)
 
-    @unittest.skip("delete this line to run the test")
+
     def test_stock_count(self):
         count = get_stock_count(self.cc_pet_shop)
         self.assertEqual(6, count)
@@ -127,24 +127,24 @@ class TestPetShop(unittest.TestCase):
         pet = find_pet_by_name(self.cc_pet_shop, "Arthur")
         self.assertEqual("Arthur", pet["name"])
 
-    @unittest.skip("delete this line to run the test")
+
     def test_find_pet_by_name__returns_None(self):
         pet = find_pet_by_name(self.cc_pet_shop, "Fred")
         self.assertIsNone(pet)
+   
 
-    @unittest.skip("delete this line to run the test")
     def test_remove_pet_by_name(self):
         remove_pet_by_name(self.cc_pet_shop, "Arthur")
         pet = find_pet_by_name(self.cc_pet_shop,"Arthur")
         self.assertIsNone(pet)
 
-    @unittest.skip("delete this line to run the test")
+ 
     def test_add_pet_to_stock(self):
         add_pet_to_stock(self.cc_pet_shop, self.new_pet)
         count = get_stock_count(self.cc_pet_shop)
         self.assertEqual(7, count)
 
-    @unittest.skip("delete this line to run the test")
+
     def test_customer_cash(self):
         cash = get_customer_cash(self.customers[0])
         self.assertEqual(1000, cash)
@@ -155,12 +155,12 @@ class TestPetShop(unittest.TestCase):
         remove_customer_cash(customer, 100)
         self.assertEqual(900, customer["cash"])
 
-    @unittest.skip("delete this line to run the test")
+  
     def test_customer_pet_count(self):
         count = get_customer_pet_count(self.customers[0])
         self.assertEqual(0, count)
 
-    @unittest.skip("delete this line to run the test")
+   
     def test_add_pet_to_customer(self):
         customer = self.customers[0]
         add_pet_to_customer(customer, self.new_pet)
